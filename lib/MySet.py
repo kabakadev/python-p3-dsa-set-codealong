04fc69c1a97c4b921855fcd6bdfdc3bad7ed56d6
Ian Kabaka <iankabaka1@gmail.com>
2024-12-12 23:05:52 +0300

    passing tess for python dsa set codealong

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,20 @@
 class MySet:
-
-    pass
+    def __init__(self,enumarable = []):
+        self.dictionary = {}
+        for value in enumarable:
+            self.dictionary[value] = True
+    def has(self,value):
+        return value in self.dictionary
+    def add(self,value):
+        self.dictionary[value] = True
+        return self
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+    def size(self):
+        return len(self.dictionary)
+    def clear(self):
+        self.dictionary.clear()
+        return self.dictionary
+    def __str__(self):
+       return f"MySet: {{{','.join(map(str, sorted(self.dictionary)))}}}"
